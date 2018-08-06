f237add686186713b83e35b5a6a4777b0940fb14
“jfelder555” <thejumbalia@gmail.com>
2018-08-06 15:05:42 -0400

    added fname lname display

---
+++ displayNames/js.js
@@ -0,0 +1,7 @@
+<script>
+			function getFormValue(){
+			var fname = document.getElementById('fname').value;
+			var lname = document.getElementById('lname').value;
+			document.getElementById('display').innerHTML = "Hello " + fname + lname;
+			}
+		</script>
